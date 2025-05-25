0e39ece5382c24445093f036ecaa8c169e097b22
ja153903 <jaimeabbariao@gmail.com>
2025-05-25 17:13:37 -0400

    improving to use reduce instead of a loop

--- lib/arrays.ts
+++ lib/arrays.ts
@@ -1,9 +1,3 @@
 export function sum<T>(items: T[], getNumber: (a: T) => number) {
-	let result = 0;
-
-	for (const item of items) {
-		result += getNumber(item);
-	}
-
-	return result;
+	return items.reduce((acc, item) => acc + getNumber(item), 0);
 }
